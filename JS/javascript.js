0dd73cad75bef479f361cfb643ca2f5b8ce0721c
marypaz-flores <amarypaz.flores@gmail.com>
2024-07-23 21:32:50 +0200

    feat: JS code to display date & time based on the city selected

--- JS/javascript.js
+++ JS/javascript.js
@@ -2,25 +2,52 @@ function updateTime() {
   //Mexico City
   let mexicoElement = document.querySelector("#mexico-city");
 
-  let mexicoDateElement = mexicoElement.querySelector(".date");
-  let mexicoTimeElement = mexicoElement.querySelector(".time");
-  let mexicoTime = moment().tz("America/Mexico_City");
+  if (mexicoElement) {
+    let mexicoDateElement = mexicoElement.querySelector(".date");
+    let mexicoTimeElement = mexicoElement.querySelector(".time");
+    let mexicoTime = moment().tz("America/Mexico_City");
 
-  mexicoDateElement.innerHTML = mexicoTime.format("MMMM Do YYYY");
-  mexicoTimeElement.innerHTML = mexicoTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    mexicoDateElement.innerHTML = mexicoTime.format("MMMM Do YYYY");
+    mexicoTimeElement.innerHTML = mexicoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Paris
   let parisElement = document.querySelector("#paris");
 
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
 
-  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+        </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
